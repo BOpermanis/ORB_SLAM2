78552c42d5b3d2c95b1b336b1c9fc7820b9245c7
bruno <bruno.opermanis@gmail.com>
2020-03-20 22:28:16 +0200

    generate aruco fixed

--- src/generate_markers.cpp
+++ src/generate_markers.cpp
@@ -2,9 +2,9 @@
 // Created by root on 2/8/20.
 //
 
-#include<opencv2/core/core.hpp>
-#include<opencv2/aruco.hpp>
+//#include<opencv2/core/core.hpp>
+//#include<opencv2/aruco.hpp>
 
-cv::Mat markerImage;
-cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-cv::aruco::drawMarker(dictionary, 23, 200, markerImage, 1);
+//cv::Mat markerImage;
+//cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+//cv::aruco::drawMarker(dictionary, 23, 200, markerImage, 1);
